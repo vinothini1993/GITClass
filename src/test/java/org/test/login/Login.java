05326b78a132c5e1d1347053a50709648df060cc
vinothini1993 <vinothini133@gmail.com>
2022-03-09 11:34:04 +0530

    Login

---
+++ src/test/java/org/test/login/Login.java
@@ -0,0 +1,23 @@
+package org.test.login;
+
+public class Login {
+	public void method1() {
+		System.out.println("Method: 1");
+
+	}
+	public void method2() {
+		System.out.println("Method: 2");
+	}
+	public void method3() {
+		System.out.println("Method: 3");
+
+	}
+	public void method4() {
+		System.out.println("Method: 4");
+	}
+	public void method5() {
+		System.out.println("Method: 5");
+
+	}
+
+}
